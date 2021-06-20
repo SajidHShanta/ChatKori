9343b9694400e6b7a8232b7da3e6a7716bf1580f
sajidHshanta <sajid.shanta@northsouth.edu>
2021-06-20 23:35:19 +0600

    connect MongoDB database with mongoose

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
 
+//set view engine
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
+
+//databasae connection
+mongoose.connect('mongodb://localhost:27017/chatkori', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
