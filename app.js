f631d78306b63fa17e368a3a7491ebcd6a7a854b
sajidHshanta <sajid.shanta@northsouth.edu>
2021-06-22 19:48:53 +0600

    add scripts in package.json

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ app.get('/', (req,  res) => {
   res.send('Hello World!')
 });
 
-app.listen(3000, function() {
-  console.log('Server started on port 3000');
+app.listen(process.env.PORT, function() {
+  console.log('Server started on port '+process.env.PORT);
 });
